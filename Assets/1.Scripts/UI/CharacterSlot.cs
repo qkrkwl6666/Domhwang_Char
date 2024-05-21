dd8880789ee4be77f15770543a0461c981fc2bfa
박재홍 <qkrkwl6666@gmail.com>
2024-05-21 21:29:21 +0900

    캐릭터 선택 창에서 캐릭터 레벨 UI 추가

--- Assets/1.Scripts/UI/CharacterSlot.cs
+++ Assets/1.Scripts/UI/CharacterSlot.cs
@@ -1,18 +1,28 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CharacterSlot : MonoBehaviour
 {
     public CharacterInfo characterInfo;
     public Sprite characterImage;
+    public TextMeshProUGUI levelText;
 
     public static event Action<CharacterInfo> OnCharacterUIInfo;
     public static event Action<CharacterInfo, CharacterSlot> OnCharacterUISelect;
 
+    private void OnEnable()
+    {
+        //if (characterInfo == null) return;
+        //levelText.text = characterInfo.Level.ToString();
+    }
+
     public void SetData(CharacterInfo data)
     {
+        characterInfo = data;
+
         characterInfo.Id = data.Id;
         characterInfo.Name = data.Name;
         characterInfo.Tier = data.Tier;
@@ -21,6 +31,9 @@ public class CharacterSlot : MonoBehaviour
         characterInfo.Run = data.Run;
         characterInfo.Run_Up = data.Run_Up;
         characterInfo.Skill_Id = data.Skill_Id;
+        characterInfo.Level = data.Level;
+
+        levelText.text = data.Level.ToString();
     }
 
     public void CharacterOnClick()
