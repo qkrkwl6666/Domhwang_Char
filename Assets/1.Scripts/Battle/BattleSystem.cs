8938fcb8207e6f2dc9371441159368cbe9749908
박재홍 <qkrkwl6666@gmail.com>
2024-05-30 11:47:03 +0900

    3스테이지 보스 피봇 수정 배틀 시스템 라운드 넘어가는 시간 조절

--- Assets/1.Scripts/Battle/BattleSystem.cs
+++ Assets/1.Scripts/Battle/BattleSystem.cs
@@ -127,7 +127,7 @@ public class BattleSystem : MonoBehaviour
             }
         }
 
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(2.5f);
 
         UIManager.Instance.OpenUI(Page.LOSE);
 
@@ -142,7 +142,7 @@ public class BattleSystem : MonoBehaviour
 
     IEnumerator WaitFor2Sec()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1f);
     }
 
     IEnumerator WaitForCharactersIdle(bool firstRound = false, int currentRound = -1)
