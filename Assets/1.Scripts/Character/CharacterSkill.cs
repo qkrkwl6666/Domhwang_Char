72d5c77a37b84bbda11bbb78fd97894a55ca8f62
박재홍 <qkrkwl6666@gmail.com>
2024-06-10 10:03:28 +0900

    빌드 설정 변경

--- Assets/1.Scripts/Character/CharacterSkill.cs
+++ Assets/1.Scripts/Character/CharacterSkill.cs
@@ -1,10 +1,7 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.TextCore.Text;
-using static UnityEngine.GraphicsBuffer;
 
 public class CharacterSkill : MonoBehaviour
 {
--- Assets/1.Scripts/UI/CharacterBook.cs
+++ Assets/1.Scripts/UI/CharacterBook.cs
@@ -1,8 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
 
 public class CharacterBook : MonoBehaviour
